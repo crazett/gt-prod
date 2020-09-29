01ab66b1e5e999398881a52f41666aa638f58c44
FlightlessMango <flightlessmangoyt@gmail.com>
2020-09-29 13:14:25 +0200

    Making lot's of changers to Gts table

--- db/migrate/20200923165651_create_gts.rb
+++ db/migrate/20200923165651_create_gts.rb
@@ -2,27 +2,33 @@ class CreateGts < ActiveRecord::Migration[6.0]
   def change
     create_table :gts do |t|
       t.timestamps
-      t.datetime :order_date
-      t.datetime :deliver_date
-      t.string     :customer
-      t.bigint     :ref_num
+      t.datetime   :order_date
+      t.datetime   :deliver_date
+      t.integer    :customer_id
+      t.string     :ref_num
       t.integer    :length
       t.integer    :amount
-      t.string     :width_degree
-      t.boolean    :rivet
+      t.integer    :width
+      t.integer    :degree
+      t.boolean    :rivet, default: true
       t.boolean    :under_steel
-      t.integer    :quality_id
+      t.string     :quality
       t.boolean    :precut_gt
       t.integer    :precut_extra_steel
-      t.string     :etching
+      t.string     :etching, default: "Standard"
       t.boolean    :pipe
       t.boolean    :packing
       t.boolean    :gt_tool
       t.boolean    :gt_flexo
       t.boolean    :gt_engraving
       t.text       :comment
-      t.string     :worker
-      t.string     :approver
+      t.integer    :worker_id
+      t.integer    :approver_id
+      t.integer    :priority, default: 1
+      t.boolean    :under_bool
+      t.boolean    :precut_gt_bool
+      t.boolean    :precut_extra_bool
+      t.boolean    :done, default: false
     end
   end
 end
